2ceb072a686f1459020062d718feda2a4766e816
Roman Prokhorov <i@rprokhorov.ru>
2017-09-21 00:33:39 +0300

    Rewrite script. Now it shows 6 search results.

--- rename.py
+++ rename.py
@@ -1,65 +1,55 @@
-# -*- coding: cp1251 -*-
-import requests, sys, os, logging, datetime, string, shutil
-
-from HTMLParser import HTMLParser
-from xml.dom.minidom import parseString
-
-path = u'/Volumes/D/Video/Films/'
+import requests, sys, os, logging, datetime, string, shutil, re
+from lxml import html
+# Mac OS X Path
+#path = u'/Volumes/D/Video/Films/Rename/'
+# Windows Path
+path = u'Z:\Video\Films\Rename'
+pathnew = u'Z:\Video\Films'
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'
+      }
 onlyfiles = os.listdir(path)
 files = onlyfiles
 countOfFiles = len(files)
-
-def search( FileName, EditedFileName ):
-	if len(EditedFileName) > 2:
-		print "[E] %s " %EditedFileName
-		r = requests.get("http://www.kinopoisk.ru/index.php?first=no&what=&kp_query="+EditedFileName)
+print(countOfFiles)
+def search(FileName, EditedFileName):
+	if len (EditedFileName) > 0:
+		print ("[E] %s " %EditedFileName)
+		r = requests.get("http://www.kinopoisk.ru/index.php?first=no&what=&kp_query="+EditedFileName, headers = headers)
 	else:
-		print "[F] %s " %FileName
-		r = requests.get("http://www.kinopoisk.ru/index.php?first=no&what=&kp_query="+FileName)
+		print ("[F] %s " %FileName)
+		r = requests.get("http://www.kinopoisk.ru/index.php?first=no&what=&kp_query="+FileName, headers = headers)
+	# FilmBlock = С‡Р°СЃС‚СЊ html С„Р°Р№Р»Р° СЃ РёРЅС„РѕСЂРјР°С†РёРµР№ Рѕ С„РёР»СЊРјРµ
+	tree = html.fromstring(r.content)
+	link = tree.xpath('//div[@class = "info"]/p/a/@href')[0]
+	title_array = []
+	for el in range(0, 5):
+			# RusName - the name of the movie in Russian
+			RusName = re.sub('[^0-9а-яА-Яa-zA-Z\ \(\)\-\,\.]','',tree.xpath('//div[@class = "info"]/p/a/text()')[el])
+			# EnName - the name of the movie in English
+			EngName = re.sub('[^0-9а-яА-Яa-zA-Z\ \(\)\-\,\.]','',tree.xpath('//div[@class = "info"]/span/text()')[el]).split(", ")
+			del EngName[-1]
+			EngName = ''.join(EngName)
+			# Year - movie release date
+			Year = tree.xpath('//div[@class = "info"]/p/span/text()')[el]
+			Duration = re.sub('[^0-9а-яА-Яa-zA-Z\ \(\)\-\,\.]','',tree.xpath('//div[@class = "info"]/span/text()')[el]).split(", ")[-1]
+			ext = FileName.split(".")[-1]
+			NewName = RusName+' ('+EngName+') ['+Duration+'] '+Year+'.'+ext
+			print ("["+str(el+1)+"] "+NewName)
+			title_array.append(NewName)
 
-	# FilmBlock = часть html файла с информацией о фильме
-	content = r.content.decode('cp1251')
-	FilmBlock = content[(content.find('element most_wanted')):]
-	FilmBlock = FilmBlock[:-(len(FilmBlock)-2000)]
+	answer = input("Rename (1-6). Escape (n). Edit file name (e): ")
+	if answer == "1" or answer == "2" or answer == "3" or answer == "4" or answer == "5" or answer == "6":
+		print (title_array[int(answer)-1])
+		print ("Renaming")
+		shutil.move(path+"\\"+FileName,pathnew+"\\"+title_array[int(answer)-1])
+		print ("Done!")
+	if answer == "n" or answer == "N":
+		print ("Escaped.")
+	if answer == "e" or answer == "E":
+		print ("Old file name: %s" %FileName)
+		EditFileName = input("New search name = ")
+		search(FileName = FileName,EditedFileName =EditFileName)
 
-	# Выдираем информацию о фильме
-	# RusName = Название фильма на русском
-	RusName = FilmBlock[(FilmBlock.find('<div class="info">'))+50:]
-	RusName = RusName[:-(len(RusName)-100)]
-	RusName = RusName[(RusName.find('">'))+2:]
-	RusName = RusName[:-(len(RusName)-(RusName.find('</a> <span class="year')))]
-	RusName = RusName.replace('&nbsp;',' ')
-
-	# EngName = Название фильма на английском
-	EngName = FilmBlock[(FilmBlock.find('<span class="year">'))+19:]
-	EngName = EngName[(EngName.find('<span class="gray">'))+19:]
-	EngName = EngName[:-(len(EngName)-(EngName.find('</span>'))+9)]
-	EngName = EngName.replace('&nbsp;',' ')
-	
-	# year = год выхода фильма
-	year = FilmBlock[(FilmBlock.find('<span class="year">'))+19:]
-	year = year[:-(len(year)-4)]
-
-	# ext = расширение исходного файла
-	ext = FileName[(len(FileName)-4):]
-	NewName = RusName+' ('+EngName+') '+year+ext
-
-	# Выводим название название фильма с kinopoisk.ru в консоль
-	print "[S] %s " %NewName
-	if NewName == FileName: #and NewName.decode() != EditedFileName.decode():
-		print "Фильм соответствует формату"
-	else:
-		answer = raw_input("Rename? (y/n) Edit file name (e): ")
-		if answer == "y" or answer == "Y":
-			print "Переименовываю"
-			shutil.move(path+FileName,path+NewName)
-			print "Переименовал"
-		if answer == "n" or answer == "N":
-			print "Не переименовываю"
-		if answer == "e" or answer == "E":
-			print "Old file name: %s" %FileName
-			EditFileName = raw_input("New search name = ")
-			search(FileName = FileName,EditedFileName =EditFileName)
-
-for i in range(1, countOfFiles):
+for i in range(0, countOfFiles):
 	search (FileName = files[i], EditedFileName = "")
